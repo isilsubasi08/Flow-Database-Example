d82904474b43ecc5ce50834f1711026788105521
isilsubasi19 <isil.subasi19@gmail.com>
2023-10-31 16:11:03 +0300

    Added Layout

---
+++ app/src/main/java/com/isilsubasi/flowdatabase/db/ContactsDao.kt
@@ -0,0 +1,19 @@
+package com.isilsubasi.flowdatabase.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.isilsubasi.flowdatabase.utils.Constants.CONTACTS_TABLE
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ContactsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveContacts(contactsEntity: ContactsEntity)
+
+    @Query("SELECT * FROM $CONTACTS_TABLE")
+    fun getAllContacts() : Flow<MutableList<ContactsEntity>>
+
+}
---
+++ app/src/main/java/com/isilsubasi/flowdatabase/ui/addFragment/AddContactFragment.kt
@@ -0,0 +1,22 @@
+package com.isilsubasi.flowdatabase.ui.addFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.isilsubasi.flowdatabase.R
+
+class AddContactFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_contact, container, false)
+    }
+
+
+}
