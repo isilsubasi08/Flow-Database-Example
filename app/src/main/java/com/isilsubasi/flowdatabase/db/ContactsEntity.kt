638f90cee7e2828a0d372d23183e4d192330ea94
isilsubasi19 <isil.subasi19@gmail.com>
2023-10-31 15:44:51 +0300

    Added ContactsEntity

---
+++ app/src/main/java/com/isilsubasi/flowdatabase/db/ContactsEntity.kt
@@ -0,0 +1,13 @@
+package com.isilsubasi.flowdatabase.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.isilsubasi.flowdatabase.utils.Constants.CONTACTS_TABLE
+
+@Entity(tableName = CONTACTS_TABLE)
+data class ContactsEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id : Int,
+    var name : String,
+    var phone : String
+)
