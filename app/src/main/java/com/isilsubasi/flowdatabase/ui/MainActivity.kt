fea4c98e8800e1771db9a7bbd071e282d6257715
isilsubasi19 <isil.subasi19@gmail.com>
2023-11-01 15:41:21 +0300

    Contact Fragment

--- app/src/main/java/com/isilsubasi/flowdatabase/ui/MainActivity.kt
+++ app/src/main/java/com/isilsubasi/flowdatabase/ui/MainActivity.kt
@@ -3,10 +3,28 @@ package com.isilsubasi.flowdatabase.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.isilsubasi.flowdatabase.R
+import com.isilsubasi.flowdatabase.databinding.ActivityMainBinding
+import com.isilsubasi.flowdatabase.ui.addFragment.AddContactFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            //setSupportActionBar(toolbar)
+
+            btnShowDialog.setOnClickListener {
+                AddContactFragment().show(supportFragmentManager,AddContactFragment().tag)
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/isilsubasi/flowdatabase/ui/addFragment/AddContactFragment.kt
+++ app/src/main/java/com/isilsubasi/flowdatabase/ui/addFragment/AddContactFragment.kt
@@ -1,21 +1,74 @@
 package com.isilsubasi.flowdatabase.ui.addFragment
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
 import com.isilsubasi.flowdatabase.R
+import com.isilsubasi.flowdatabase.databinding.FragmentAddContactBinding
+import com.isilsubasi.flowdatabase.db.ContactsEntity
+import com.isilsubasi.flowdatabase.viewmodel.DatabaseViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AddContactFragment : DialogFragment() {
+
+
+    @Inject
+    lateinit var entity: ContactsEntity
+
+    private val viewModel : DatabaseViewModel by viewModels()
+
+    private var contactId = 0
+    private var name=""
+    private var phone=""
+
+    private lateinit var binding: FragmentAddContactBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_contact, container, false)
+        binding=FragmentAddContactBinding.inflate(inflater,container,false)
+        dialog!!.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            imgClose.setOnClickListener {
+                dismiss()
+            }
+            btnSave.setOnClickListener {
+                name=edtName.text.toString()
+                phone=edtPhone.text.toString()
+
+                if (name.isEmpty() || phone.isEmpty()){
+                    Snackbar.make(it,"Name and phone cannot be empty",Snackbar.LENGTH_SHORT).show()
+                }
+                else{
+                    entity.id=contactId
+                    entity.name=name
+                    entity.phone=phone
+
+                    viewModel.saveContacts(entity)
+                    edtName.setText("")
+                    edtPhone.setText("")
+                    dismiss()
+
+                }
+
+            }
+        }
     }
 
 
